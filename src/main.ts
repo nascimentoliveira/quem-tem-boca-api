308b3fe7e5fc4168f3de08366beea4ab73aecb59
Thiago Oliveira <nascimentoliveira@gmail.com>
2023-09-10 20:31:51 -0300

    feat: validation body

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { loadEnv } from './config/envs.config';
+import {
+  HttpException,
+  HttpStatus,
+  ValidationError,
+  ValidationPipe,
+} from '@nestjs/common';
 
 async function bootstrap() {
   loadEnv();
   const app = await NestFactory.create(AppModule);
+  const validationExceptionFactory = (errors: ValidationError[]) => {
+    const errorMessages = errors.flatMap((error) =>
+      Object.values(error.constraints),
+    );
+    return new HttpException(
+      {
+        statusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+        message: errorMessages,
+        error: 'Unprocessable Entity',
+      },
+      HttpStatus.UNPROCESSABLE_ENTITY,
+    );
+  };
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      exceptionFactory: validationExceptionFactory,
+    }),
+  );
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
   console.log(`Server is running on port ${port}`);
