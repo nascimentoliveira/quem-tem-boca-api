183601d93823dee344ea827dd85d26da38d1bfd6
Thiago Oliveira <nascimentoliveira@gmail.com>
2023-09-23 22:45:00 -0300

    feat: update API description in swagger

--- src/config/swagger.config.ts
+++ src/config/swagger.config.ts
@@ -2,7 +2,14 @@ import { DocumentBuilder } from '@nestjs/swagger';
 
 export const swaggerConfig = new DocumentBuilder()
   .setTitle('Quem-tem-boca-API')
-  .setDescription('Documentação de Quem-tem-boca-API')
+  .setDescription(
+    `Welcome to the Quem tem boca API! 
+    This API provides access to the resources of our delivery application.
+    With this API, you can perform various operations, including registering 
+    and managing users, establishments, dishes, drinks and much more. 
+    Explore available operations and make the most of our platform.
+    See the documentation below for detailed information on how to use the API and available features.`,
+  )
   .addBearerAuth()
   .setVersion('1.0')
   .build();
